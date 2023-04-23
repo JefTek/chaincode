22f70d026d380a0f781997a8910a0a0d4e345442
Jef Kazimer <jef@jeftek.com>
2023-04-23 16:25:24 -0500

    Updated favicons

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230423.160234';
+const cacheName = 'chirpy-20230423.162410';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
