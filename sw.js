0f8a411b34a9b24655b1311e3d85dc242a6ad55b
Jef Kazimer <jef@jeftek.com>
2023-04-23 16:56:58 -0500

    Added Giscus

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230423.162410';
+const cacheName = 'chirpy-20230423.165607';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
