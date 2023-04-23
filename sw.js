0f060d4a05c122dd7a67996fe3e045bd8e880b10
Jef Kazimer <jef@jeftek.com>
2023-04-23 16:05:24 -0500

    Updated

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230423.154344';
+const cacheName = 'chirpy-20230423.160234';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
